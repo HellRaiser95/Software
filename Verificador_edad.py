48a371c9acda634c46b366389114944609efcc5a
HellRaiser95 <113647616+HellRaiser95@users.noreply.github.com>
2022-09-16 20:12:39 -0500

    Add files via upload

---
+++ Verificador_edad.py
@@ -0,0 +1,6 @@
+print("bienvenido al verificador de mayoria de edad")
+Edad=int(input("Por favor digite su edad\n"))
+if(Edad>=18):
+  print("usted es mayor de edad")
+else:
+  print("usted es menor de edad")
